aa52460373d0bbb07041971badf528c105cc8cba
Your Name <you@example.com>
2025-08-19 11:22:10 -0300

    Excluir spec

--- cypress/e2e/home.spec.cy.js
+++
@@ -1,8 +0,0 @@
-describe('Acesso pagina inicial', () => {
-  it('Visita o site e verifica título', () => {
-    cy.visit('http://localhost:3000/')
-    cy.title().should('include', 'Home')
-
-  })
-  
-});
