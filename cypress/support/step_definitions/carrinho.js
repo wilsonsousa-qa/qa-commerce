54d547781fce09f7b5d8c49152338db7a8ea7737
Your Name <you@example.com>
2025-08-19 17:19:12 -0300

    Ajuste no When do bdd

--- cypress/support/step_definitions/carrinho.js
+++ cypress/support/step_definitions/carrinho.js
@@ -6,7 +6,7 @@ Given("que o usuário está na página do produto", () => {
 });
 
 When('o usuário clica no botão "Adicionar ao Carrinho"', () => {
-  cy.get(':nth-child(1) > .card > .card-body > .btn').click();    
+  cy.get(':nth-child(1) > .card > .card-body > .btn').click();
 });
 
 Then("o produto deve ser adicionado ao carrinho com sucesso", () => {
@@ -18,13 +18,17 @@ Then("o produto deve ser adicionado ao carrinho com sucesso", () => {
 // Cenário: Verificar itens no carrinho
 Given("que o usuário adicionou produtos ao carrinho", () => {
   cy.visit("http://localhost:3000/");
-  cy.get(':nth-child(2) > .nav-link').click();
+  cy.get(':nth-child(1) > .card > .card-body > .btn').click(); // adiciona o 1º produto
 });
 
-When("o usuário acessa a página do carrinho", () => {
+When("o usuário acessa diretamente a página do carrinho", () => {
+  cy.get(':nth-child(2) > .nav-link').click(); // clica no menu "Carrinho"
+  cy.url().should("include", "/cart.html"); // confirma a URL
   cy.get('h1').should('include.text', 'SEU CARRINHO');
+  cy.get('h4').should('include.text', 'Verifique seus produtos e avance para o checkout:');
 });
 
 Then("o usuário deve ver os produtos adicionados no carrinho", () => {
   cy.get('.cart-item').should('exist');
 });
+
