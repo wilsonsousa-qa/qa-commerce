eace5f90a524458aa470a622ecb5ef0c1ac61698
Your Name <you@example.com>
2025-08-19 11:17:53 -0300

    Ajustando cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,21 @@
+// cypress.config.js
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  // opções globais
+  viewportWidth: 1280,
+  viewportHeight: 720,
+  defaultCommandTimeout: 10000,
+  pageLoadTimeout: 60000,
+  chromeWebSecurity: false,
+
   e2e: {
+    baseUrl: "http://localhost:3000",
+    // mantenha seus .spec.js (ou adapte se preferir .cy.js)
+    specPattern: "cypress/e2e/**/*.spec.js",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // eventos/node plugins aqui se precisar
+      return config;
     },
   },
 });
---
+++ cypress/e2e/spec/login.feature
@@ -0,0 +1,9 @@
+language: pt
+
+Feature: Acesso Functionality
+
+    Feature testar a funcionalidade de acesso da aplicação
+    Scenario: Acessar a aplicação com sucesso
+        Given que o usuário ao pesquisar pela URL da aplicação  
+        When o usuário coloca URL no navegador
+        Then o usuário deve ser redirecionado para a página inicial
