02c07bd003ebd7e953eea21587304b964d7f0cb5
Your Name <you@example.com>
2025-08-19 14:42:23 -0300

    Ajuste de posição elementos

--- cypress/support/step_definitions/home.js
+++ cypress/support/step_definitions/home.js
@@ -1,4 +1,4 @@
-import { Given, When, And, Then } from "cypress-cucumber-preprocessor/steps";
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
 Given("que o usuário ao pesquisar pela URL da aplicação", () => {
   cy.visit("http://localhost:3000/");
@@ -6,9 +6,9 @@ Given("que o usuário ao pesquisar pela URL da aplicação", () => {
 
 When("o usuário coloca URL no navegador", () => {
   cy.visit("http://localhost:3000/");
-}); 
+});
 
 Then("o usuário deve ser redirecionado para a página inicial", () => {
   cy.title().should("include", "Home");
-  cy.get("h1").should("include.text", "QA Commerce");   
+  cy.get("h1").should("include.text", "QA Commerce");
 });
