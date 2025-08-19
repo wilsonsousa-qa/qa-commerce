64408d7e0eb721c1fddd85338621784641bd2421
Your Name <you@example.com>
2025-08-19 15:03:10 -0300

    Ajuste config js

--- cypress.config.js
+++ cypress.config.js
@@ -1,18 +1,15 @@
 const { defineConfig } = require("cypress");
 const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
 const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
-const { createEsbuildPlugin } = require("@badeball/cypress-cucumber-preprocessor/esbuild");
+const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild");
 
 module.exports = defineConfig({
   e2e: {
-    specPattern: "cypress/e2e/**/*.feature",
+    specPattern: "cypress/e2e/features/**/*.feature",
     setupNodeEvents(on, config) {
-      on(
-        "file:preprocessor",
-        createBundler({
-          plugins: [createEsbuildPlugin(config)],
-        })
-      );
+      on("file:preprocessor", createBundler({
+        plugins: [createEsbuildPlugin.default(config)],
+      }));
       preprocessor.addCucumberPreprocessorPlugin(on, config);
       return config;
     },
