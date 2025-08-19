d536b3603bc6a4e88a5cf24f5c854b8ffd09396a
Your Name <you@example.com>
2025-08-18 22:02:42 -0300

    Bdd ajustados

--- cypress/e2e/pages/home.page.cy.js
+++ cypress/e2e/pages/home.page.cy.js
@@ -1,8 +1,11 @@
-describe('Acesso pagina inicial', () => {
-  it('Visita o site e verifica título', () => {
-    cy.visit('http://localhost:3000/')
-    cy.title().should('include', 'Home')
+class HomePage {
+  visit() {
+    cy.visit('http://localhost:3000/');
+  }
 
-  })
-  
-});
+  verificarTitulo(textoEsperado) {
+    cy.title().should('include', textoEsperado);
+  }
+}
+
+export default HomePage;
--- cypress/e2e/steps/home.steps.js
+++ cypress/e2e/steps/home.steps.js
@@ -1,13 +1,18 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+import HomePage from "../../pages/home.page";
+
+const homePage = new HomePage();
 
 Given('que eu acesso a página inicial', () => {
-  cy.visit('http://localhost:3000/');
+  homePage.visit();
 });
 
 When('a página carrega', () => {
-
+  // Aqui podemos colocar checagens adicionais de carregamento
+  cy.get('body').should('be.visible'); 
 });
 
-Then('eu devo ver o título {string}', (Home) => {
-  cy.title().should('include', Home);
+Then('eu devo ver o título {string}', (titulo) => {
+  homePage.verificarTitulo(titulo);
 });
+
