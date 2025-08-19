ba8bb43f0dfbe99fb3e2e9cec40eee3cd6d92200
Your Name <you@example.com>
2025-08-19 10:02:44 -0300

    Criado base Url

--- cypress/e2e/spec/home.page.spec.js
+++ cypress/e2e/spec/home.page.spec.js
@@ -2,6 +2,7 @@ describe('Acesso pagina inicial', () => {
   it('Visita o site e verifica título', () => {
     cy.visit('http://localhost:3000/')
     cy.title().should('include', 'Home')
+    "testFiles";"**/*.feature"
 
   })
   
